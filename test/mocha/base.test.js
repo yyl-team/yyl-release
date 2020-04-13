7fd5961eb52ff15a29361d3fa9e78fe6f172971b
liudaojie <liudaojie@yy.com>
2020-04-13 17:32:44 +0800

    feat: test case finished

--- test/mocha/base.test.js
+++ test/mocha/base.test.js
@@ -9,7 +9,7 @@ const PJ_PATH = path.join(GITCASE_PATH, 'yyl-release-test')
 
 const changeVersion = (str) => {
   const arr = str.split('.')
-  return `${arr.slice(0, arr.length - 1).join('.')}.${arr.pop() + 1}`
+  return `${arr.slice(0, arr.length - 1).join('.')}.${+arr.pop() + 1}`
 }
 describe('usage test', () => {
   beforeEach(async () => {
@@ -17,9 +17,11 @@ describe('usage test', () => {
       await extOs.runCMD('git reset --hard', PJ_PATH)
       await extOs.runCMD('git checkout test', PJ_PATH)
     } else {
+      await extFs.removeFiles(GITCASE_PATH, true)
       await extFs.mkdirSync(GITCASE_PATH)
       await extOs.runCMD(
-        'git clone https://github.com/jackness1208/yyl-release-test.git'
+        'git clone https://github.com/jackness1208/yyl-release-test.git',
+        GITCASE_PATH
       )
     }
   })
@@ -32,6 +34,7 @@ describe('usage test', () => {
     const pkg = require(pkgPath)
     pkg.version = changeVersion(pkg.version)
     fs.writeFileSync(editPath, `${editCnt}\nconsole.log('test ${+new Date()}')`)
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
     await release({ target: PJ_PATH })
   })
 })
