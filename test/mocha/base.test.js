48ba0238a44891140f66c2b35bd2e49abefb2231
liudaojie <liudaojie@yy.com>
2020-04-13 17:27:11 +0800

    feat: 新增测试用例

---
+++ test/mocha/base.test.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const fs = require('fs')
+const { release } = require('../../')
+const extOs = require('yyl-os')
+const extFs = require('yyl-fs')
+
+const GITCASE_PATH = path.join(__dirname, '../__gitcase')
+const PJ_PATH = path.join(GITCASE_PATH, 'yyl-release-test')
+
+const changeVersion = (str) => {
+  const arr = str.split('.')
+  return `${arr.slice(0, arr.length - 1).join('.')}.${arr.pop() + 1}`
+}
+describe('usage test', () => {
+  beforeEach(async () => {
+    if (fs.existsSync(PJ_PATH)) {
+      await extOs.runCMD('git reset --hard', PJ_PATH)
+      await extOs.runCMD('git checkout test', PJ_PATH)
+    } else {
+      await extFs.mkdirSync(GITCASE_PATH)
+      await extOs.runCMD(
+        'git clone https://github.com/jackness1208/yyl-release-test.git'
+      )
+    }
+  })
+
+  it('git ctrl test', async () => {
+    /** 修改 文件 */
+    const editPath = path.join(PJ_PATH, 'index.js')
+    const pkgPath = path.join(PJ_PATH, 'package.json')
+    const editCnt = fs.readFileSync(editPath).toString()
+    const pkg = require(pkgPath)
+    pkg.version = changeVersion(pkg.version)
+    fs.writeFileSync(editPath, `${editCnt}\nconsole.log('test ${+new Date()}')`)
+    await release({ target: PJ_PATH })
+  })
+})
