edcb0fca4cbfe3378f0633963b26d976ff1b2c06
liudaojie <liudaojie@yy.com>
2020-04-13 13:21:28 +0800

    feat: 开发完成

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const fs = require('fs')
+const extOs = require('yyl-os')
+
 const { LANG } = require('./lang/index')
-module.exports.release = ({ target }) => {
+module.exports.release = async ({ target }) => {
   const pkgPath = path.join(target, 'package.json')
   if (fs.existsSync(pkgPath)) {
     throw new Error(LANG.PKG_NOT_EXISTS)
@@ -13,4 +15,45 @@ module.exports.release = ({ target }) => {
     },
     pkg.yyr
   )
+
+  /** 执行npm publish */
+  await extOs.runCMD('npm publish', target)
+
+  const { version } = pkg
+  /** 获取当前 分支 */
+  const curBranch = (await extOs.runCMD('git branch --contains', target, false))
+    .trim()
+    .replace(/^\* /, '')
+  print.log.info(`当前分支 ${curBranch}`)
+
+  /** 提交 */
+  const changefiles = (await extOs.runCMD('git status -s', target)).trim()
+
+  // 有更新
+  if (changefiles.length) {
+    print.log.info(`提交当前分支(${curBranch}) 开始`)
+    await extOs.runCMD('git add .', target)
+    await extOs.runCMD(`git commit -m "build: release ${version}"`, target)
+    await extOs.runCMD('git push', target)
+    print.log.success(`提交当前分支(${curBranch}) 完成`)
+  }
+
+  /** 合并到 master */
+  if (curBranch !== 'master') {
+    print.log.info(`合并当前分支(${curBranch}) 到 master 开始`)
+    await extOs.runCMD('git checkout master', target)
+    await extOs.runCMD(`git pull`, target)
+    await extOs.runCMD(`git merge ${curBranch}`, target)
+    await extOs.runCMD('git push', target)
+    print.log.success(`合并当前分支(${curBranch}) 到 master 完成`)
+  }
+
+  if (option.tag) {
+    await extOs.runCMD(
+      `git tag -a v${version} -m "build: release ${version}"`,
+      target
+    )
+    print.log.success(`新建 v${version} tag 完成`)
+  }
+  print.log.success(`all is done`)
 }
--- lang/index.js
+++ lang/index.js
@@ -1,3 +1,10 @@
 module.export.LANG = {
-  PKG_NOT_EXISTS: 'package.json 不存在'
+  PKG_NOT_EXISTS: 'package.json 不存在',
+  PUSH_CUR_BRANCH_START: '提交当前分支 开始',
+  PUSH_CUR_BRANCH_FINISHED: '提交当前分支 完成',
+  MERGE_TO_MASTER_START: '合并分支到 master 开始',
+  MERGE_TO_MASTER_FINISHED: '合并分支到 master 完成',
+  BUILD_TAG_START: '新建标签 开始',
+  BUILD_TAG_FINISHED: '新建标签 完成',
+  ALL_IS_DONE: '任务已完成'
 }
