ad6bfdfda2bc69f1b35abd9d1d9e847469312f3d
liudaojie <liudaojie@yy.com>
2020-04-13 16:58:41 +0800

    feat: 完成

--- index.js
+++ index.js
@@ -1,59 +1,96 @@
 const path = require('path')
 const fs = require('fs')
 const extOs = require('yyl-os')
+const chalk = require('chalk')
+const print = require('yyl-print')
+
+print.log.init({
+  cmd: {
+    text: 'CMD>',
+    color: chalk.bgBlack.white
+  }
+})
 
 const { LANG } = require('./lang/index')
 module.exports.release = async ({ target }) => {
-  const pkgPath = path.join(target, 'package.json')
-  if (fs.existsSync(pkgPath)) {
-    throw new Error(LANG.PKG_NOT_EXISTS)
+  const pkgPath = path.join(path.resolve(process.cwd(), target), 'package.json')
+  if (!fs.existsSync(pkgPath)) {
+    throw new Error(`${LANG.PKG_NOT_EXISTS}: ${pkgPath}`)
   }
   const pkg = require(pkgPath)
   const option = Object.assign(
     {
-      tag: true
+      tag: true,
+      publish: true
     },
     pkg.yyr
   )
 
   /** 执行npm publish */
-  await extOs.runCMD('npm publish', target)
+  if (option.publish) {
+    print.log.cmd('npm publish')
+    await extOs.runCMD('npm publish', target)
+  }
 
   const { version } = pkg
   /** 获取当前 分支 */
   const curBranch = (await extOs.runCMD('git branch --contains', target, false))
     .trim()
     .replace(/^\* /, '')
-  print.log.info(`当前分支 ${curBranch}`)
+  print.log.info(`${LANG.CUR_BRANCH}: ${chalk.cyan(curBranch)}`)
 
   /** 提交 */
-  const changefiles = (await extOs.runCMD('git status -s', target)).trim()
+  const changefileLog = (
+    await extOs.runCMD('git status -s', target, false)
+  ).trim()
+
+  const changeTotal = changefileLog ? changefileLog.split(/[\r\n]+/g).length : 0
+  print.log.info(`${LANG.STAGED_FILES}: ${chalk.cyan(changeTotal)}`)
 
   // 有更新
-  if (changefiles.length) {
-    print.log.info(`提交当前分支(${curBranch}) 开始`)
-    await extOs.runCMD('git add .', target)
-    await extOs.runCMD(`git commit -m "build: release ${version}"`, target)
-    await extOs.runCMD('git push', target)
-    print.log.success(`提交当前分支(${curBranch}) 完成`)
+  if (changeTotal > 0) {
+    print.log.info(`${LANG.PUSH_CUR_BRANCH_START}: ${curBranch}`)
+    const cmds = [
+      'git add .',
+      `git commit -m "build: release ${version}"`,
+      'git push'
+    ]
+
+    for (let i = 0; i < cmds.length; i++) {
+      print.log.cmd(cmds[i])
+      await extOs.runCMD(cmds[i], target)
+    }
+
+    print.log.success(`${LANG.PUSH_CUR_BRANCH_FINISHED}: ${curBranch}`)
   }
 
   /** 合并到 master */
   if (curBranch !== 'master') {
-    print.log.info(`合并当前分支(${curBranch}) 到 master 开始`)
-    await extOs.runCMD('git checkout master', target)
-    await extOs.runCMD(`git pull`, target)
-    await extOs.runCMD(`git merge ${curBranch}`, target)
-    await extOs.runCMD('git push', target)
-    print.log.success(`合并当前分支(${curBranch}) 到 master 完成`)
+    print.log.info(`${LANG.MERGE_TO_MASTER_START}: ${curBranch}`)
+    const cmds = [
+      'git checkout master',
+      'git pull',
+      `git merge ${curBranch}`,
+      'git push'
+    ]
+    for (let i = 0; i < cmds.length; i++) {
+      print.log.cmd(cmds[i])
+      await extOs.runSpawn(cmds[i], target)
+    }
+    print.log.success(`${LANG.MERGE_TO_MASTER_FINISHED}: ${curBranch}`)
   }
 
   if (option.tag) {
-    await extOs.runCMD(
-      `git tag -a v${version} -m "build: release ${version}"`,
-      target
-    )
-    print.log.success(`新建 v${version} tag 完成`)
+    print.log.info(`${LANG.BUILD_TAG_START}`)
+    const cmds = [`git tag -a v${version} -m "release ${version}"`]
+
+    for (let i = 0; i < cmds.length; i++) {
+      print.log.cmd(cmds[i])
+      await extOs.runCMD(cmds[i], target).catch((er) => {
+        print.log.error(er)
+      })
+    }
+    print.log.success(`${LANG.BUILD_TAG_FINISHED}`)
   }
-  print.log.success(`all is done`)
+  print.log.success(LANG.ALL_IS_DONE)
 }
--- lang/index.js
+++ lang/index.js
@@ -1,5 +1,7 @@
-module.export.LANG = {
+module.exports.LANG = {
   PKG_NOT_EXISTS: 'package.json 不存在',
+  STAGED_FILES: '文件变更数',
+  CUR_BRANCH: '当前分支',
   PUSH_CUR_BRANCH_START: '提交当前分支 开始',
   PUSH_CUR_BRANCH_FINISHED: '提交当前分支 完成',
   MERGE_TO_MASTER_START: '合并分支到 master 开始',
